5d38dc72448b204ee310d18b31daff4fcb810ae0
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-06-18 18:35:58 +0300

    Making XHR request

--- index.js
+++ index.js
@@ -1,15 +1,39 @@
 const api_endpoint = 'https://restcountries.eu/rest/v2/all';
 
+// Helpers
+const show = selector => {
+  const element = document.querySelector(selector);
+  element.removeAttribute('hidden');
+}
+const hide = selector => {
+  const element = document.querySelector(selector);
+  element.setAttribute('hidden', '');
+}
+
 document.addEventListener("DOMContentLoaded", () => {
+  const req = new XMLHttpRequest();
+
+  // Handle response
+  const reqListener = (e) => {
+    try {
+      // Parse response
+      const json = JSON.parse(req.responseText);
+      console.log(json);
+
+      // Fill content
+
+      // Hide Spinner
+      hide('#spinner')
+
+      // Show content
+      show('#countries')
+    } catch (error) {
+      console.error('Wrong response');
+    }
+  }
+
   // Make request
-
-  // Fill content
-
-  // Hide Spinner
-  const spinnerEl = document.querySelector('#spinner');
-  spinnerEl.setAttribute('hidden', '');
-
-  // Show content
-  const countriesEl = document.querySelector('#countries');
-  countriesEl.removeAttribute('hidden');
-})
+  req.addEventListener("load", reqListener);
+  req.open("GET", api_endpoint);
+  req.send();
+});
