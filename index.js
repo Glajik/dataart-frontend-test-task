b89782eeeded6be815f8154d15f834a93aaa8d5d
Viacheslav Glazhevskyi <glajik@gmail.com>
2020-06-18 23:13:39 +0300

    Append timezone, currencies and translations to card

--- index.js
+++ index.js
@@ -17,14 +17,53 @@ const hide = selector => {
   element.setAttribute('hidden', '');
 }
 
+// Render list items
+const renderListItems = list => list.map(value => {
+    const node = document.createElement('li');
+    node.textContent = value;
+    return node;
+});
+
+const renderCurrencies = items => {
+  const strings = items.map(({ code, symbol }) => `${code} - ${symbol}`);
+  return renderListItems(strings);
+}
+
+const renderTranslations = data => {
+  const strings = Object.entries(data).map(([lang, translation]) => `${lang}: ${translation}`)
+  return renderListItems(strings);
+}
+
 // Render country card
 const renderCard = (template, props) => {
-  const { name, region, population, flag } = props;
+  const {
+    name,
+    flag,
+    region,
+    capital,
+    population,
+    timezones,
+    currencies,
+    translations
+  } = props;
+  
+  // Render list items
+  const timezoneNodes = renderListItems(timezones);
+  const currencyNodes = renderCurrencies(currencies);
+  const translationNodes = renderTranslations(translations);
+  
+  // Clone node from template
   const node = template.content.cloneNode(true);
+  
+  // Fill card content
   node.querySelector('.card img').setAttribute('src', flag);
   node.querySelector('.card-title').textContent = name;
   node.querySelector('.card-subtitle').textContent = region;
-  node.querySelector('.card-text').textContent = `Population: ${population}`;
+  node.querySelector('.card-slot-capital').textContent = capital;
+  node.querySelector('.card-slot-population').textContent = population;
+  node.querySelector('.card-slot-timezone').append(...timezoneNodes);
+  node.querySelector('.card-slot-currencies').append(...currencyNodes);
+  node.querySelector('.card-slot-translations').append(...translationNodes);
   return node;
 }
 
